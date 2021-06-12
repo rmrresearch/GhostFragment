029b90e73029533ba21bd84997b58ee817434a32
Ryan Richard <ryanmrichard1@gmail.com>
2021-06-12 12:01:41 -0500

    adds partitoned property type

--- include/ghostfragment/property_types/property_types.hpp
+++ include/ghostfragment/property_types/property_types.hpp
@@ -1,8 +1,9 @@
 #pragma once
 #include <property_types/connectivity.hpp>
+#include <property_types/topology.hpp>
 
 namespace ghostfragment::pt {
 
 using connectivity = property_types::Connectivity;
 
-}
+} // namespace ghostfragment::pt
