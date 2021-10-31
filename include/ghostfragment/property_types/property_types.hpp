148f6ca29ab3cd4345ca61840b4882758f3492cc
Ryan Richard <ryanmrichard1@gmail.com>
2021-10-31 10:05:33 -0500

    nmers hooked back up

--- include/ghostfragment/property_types/property_types.hpp
+++ include/ghostfragment/property_types/property_types.hpp
@@ -3,4 +3,13 @@
 #include "ghostfragment/types.hpp"
 #include <simde/simde.hpp>
 
-namespace ghostfragment::pt {} // namespace ghostfragment::pt
+namespace ghostfragment::pt {
+
+using frag_traits = simde::FragmentedTraits<simde::type::molecule>;
+
+using nmer_traits = simde::FragmentedTraits<typename frag_traits::return_type>;
+
+/// Property type for a module which computes N-Mers
+using NMers = simde::Fragmented<typename nmer_traits::input_type>;
+
+} // namespace ghostfragment::pt
--- src/ghostfragment/partitioned/partitioned.hpp
+++ src/ghostfragment/partitioned/partitioned.hpp
@@ -4,14 +4,14 @@
 namespace ghostfragment::partitioned {
 
 DECLARE_MODULE(Cluster);
-// DECLARE_MODULE(NMers);
+DECLARE_MODULE(NMers);
 DECLARE_MODULE(AO2Atom);
 DECLARE_MODULE(NucleiAO);
 // DECLARE_MODULE(SystemAO);
 
 inline void load_modules(pluginplay::ModuleManager& mm) {
     mm.add_module<Cluster>("Cluster Partition");
-    // mm.add_module<NMers>("All nmers");
+    mm.add_module<NMers>("All nmers");
     mm.add_module<AO2Atom>("AO Center to Atom Mapper");
     mm.add_module<NucleiAO>("Nuclei-AO Fragmenter");
     // mm.add_module<SystemAO>("System-AO Fragmenter");
