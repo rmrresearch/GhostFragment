d78e15c73e8452d4d7d17bfa97b13e35788446e4
ryan <ryanmrichard1@gmail.com>
2022-10-10 08:51:43 -0500

    refactor tests [skip ci]

--- src/ghostfragment/drivers/fragment_based_method.cpp
+++ src/ghostfragment/drivers/fragment_based_method.cpp
@@ -49,9 +49,22 @@ MODULE_RUN(FragmentBasedMethod) {
     const auto& [expr] = expr_mod.run_as<expression_pt>(nmers);
 
     // Step 4: Run those terms
+
+    // Get MPI conditions
+    auto& rt  = get_runtime();
+    auto size = rt.size();
+    auto me   = rt.my_resource_set().mpi_rank();
+
+    // Work out batch sizes
+    auto extra      = expr.size() % size; // Remainder?
+    auto batch_size = (expr.size() - extra) / size;
+    auto my_begin   = me * batch_size;
+    auto my_end     = my_begin + batch_size + (me == size - 1 ? extra : 0);
+    std::cout << me << " " << my_begin << " " << my_end << std::endl;
+
     double e      = 0.0;
     auto& egy_mod = submods.at("energy method");
-    for(std::size_t term_index = 0; term_index < expr.size(); ++term_index) {
+    for(std::size_t term_index = my_begin; term_index < my_end; ++term_index) {
         const auto& term_i = expr.at(term_index);
         const auto sys_i   = term_i.chemical_system();
         const chemist::orbital_space::AOSpace aos_i(term_i.ao_basis_set());
@@ -60,7 +73,9 @@ MODULE_RUN(FragmentBasedMethod) {
         e += c_i * e_term_i;
     }
 
-    auto rv = results();
+    auto egys = rt.gather(e);
+    e         = std::accumulate(egys.begin(), egys.end(), 0.0);
+    auto rv   = results();
     return my_pt::wrap_results(rv, e);
 }
 
---
+++ tests/integration/ghostfragment/__init__.py
@@ -0,0 +1,6 @@
+'''Contains integration tests for GhostFragment.
+'''
+from simde import pluginplay
+
+# This line is here to keep MPI alive across the test suite
+mm = pluginplay.ModuleManager()
