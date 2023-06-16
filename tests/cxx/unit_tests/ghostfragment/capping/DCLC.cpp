b84cbf1158e843704323551d763c714248da1201
Isaac <vanormani@carleton.edu>
2023-06-16 13:49:47 -0500

    Revert "	modified:   src/.DS_Store"
    This reverts commit 51deddcf6bd57d96fa9edfc6ca06126deaedbe6c.

--- tests/cxx/unit_tests/ghostfragment/capping/DCLC.cpp
+++ tests/cxx/unit_tests/ghostfragment/capping/DCLC.cpp
@@ -1,56 +1,36 @@
 #include "../test_ghostfragment.hpp"
-#include "DCLC.hpp"
 #include <ghostfragment/property_types/connectivity_table.hpp>
-#include <hydrocarbon/hydrocarbon_fragment.hpp>
+
+using namespace ghostfragment;
+using namespace testing;
+
+using the_pt      = pt::Capped;
+using connect_pt  = ConnectivityTable;
+using ct_t        = ConnectivityTableTraits::result_type;
+using return_type = pt::CappedTraits::result_type;
+using size_type   = std::size_t;
 
 TEST_CASE("DCLC Capping") {
-
-    using namespace ghostfragment;
-    using namespace testing;
-
-    using the_pt      = pt::DCLC;
-    using input_type  = chemist::FragmentedNuclei;
-    using result_type = chemist::CapSet;
-
     auto mm   = initialize();
     auto& mod = mm.at("DCLC Capping");
 
-    SECTION("Ethane (2 carbon 2 frags)") {
-        chemist::Molecule corr(Ethane2());
-        input_type hc{hydrocarbon_fragmented_nuclei(2,1)};
-        result_type caps = mod.run_as<the_pt>(hc);
-        chemist::Molecule test;
-        for(const auto& cap_i :: caps){
-            for(i = 0; i < cap_i.size(); ++i){
-                test.push_back(atom_type cap_i.cap_atom(i));
-            }
-        }
-        REQUIRE(AreMoleculesEqual(corr, test));
-    }
+    SECTION("Hydrocarbon monomers") {
+        for(size_type n_carbons = 1; n_carbons < 4; ++n_carbons) {
+            SECTION("N = " + std::to_string(n_carbons)) {
+                auto carbon_n = water_fragmented_nuclei(n_waters);
 
-    SECTION("Propane (3 carbon 2 frags)") {
-        chemist::Molecule corr(Propane2());
-        input_type hc{hydrocarbon_fragmented_nuclei(3,2)};
-        result_type caps = mod.run_as<the_pt>(hc);
-        chemist::Molecule test;
-        for(const auto& cap_i :: caps){
-            for(i = 0; i < cap_i.size(); ++i){
-                test.push_back(atom_type cap_i.cap_atom(i));
-            }
-        }
-        REQUIRE(AreMoleculesEqual(corr, test));
-    }
+                auto conn_mod =
+                  pluginplay::make_lambda<connect_pt>([=](const auto& mol_in) {
+                      REQUIRE(mol_in == water_n.supersystem());
+                      return water_connectivity(n_waters);
+                  });
 
-    SECTION("Propane (3 carbon 4 frags)") {
-        chemist::Molecule corr(Propane4());
-        input_type hc{hydrocarbon_fragmented_nuclei(3,1)};
-        result_type caps = mod.run_as<the_pt>(hc);
-        chemist::Molecule test;
-        for(const auto& cap_i :: caps){
-            for(i = 0; i < cap_i.size(); ++i){
-                test.push_back(atom_type cap_i.cap_atom(i));
+                return_type corr(n_waters);
+
+                mod.change_submod("Connectivity", conn_mod);
+                const auto& caps = mod.run_as<the_pt>(water_n);
+                REQUIRE(caps == corr);
             }
         }
-        REQUIRE(AreMoleculesEqual(corr, test));
     }
 }
