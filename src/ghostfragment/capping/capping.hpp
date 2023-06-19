c2b65ebe18acde621564fc1f96807d348a3e00ce
Walker Hayes <112342932+wthayes@users.noreply.github.com>
2023-06-19 13:22:26 -0500

    Weighted Distance Testing

--- src/ghostfragment/capping/capping.hpp
+++ src/ghostfragment/capping/capping.hpp
@@ -4,11 +4,16 @@
 namespace ghostfragment::capping {
 
 DECLARE_MODULE(SingleAtom);
+DECLARE_MODULE(WeightedDistance);
 
 inline void load_modules(pluginplay::ModuleManager& mm) {
     mm.add_module<SingleAtom>("Atomic Capping");
+
+    mm.add_module<WeightedDistance>("Weighted Distance");
 }
 
-inline void set_defaults(pluginplay::ModuleManager& mm) {}
+inline void set_defaults(pluginplay::ModuleManager& mm) {
+    mm.change_submod("Weighted Distance", "Broken Bonds", "Broken Bonds");
+}
 
 } // namespace ghostfragment::capping
