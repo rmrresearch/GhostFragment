ffe07c5e275242fb062640c039efe32a8da01637
ryan <ryanmrichard1@gmail.com>
2023-06-20 10:23:26 -0500

    rename partitioned to fragmenting

--- src/ghostfragment/ghostfragment.cpp
+++ src/ghostfragment/ghostfragment.cpp
@@ -2,7 +2,7 @@
 #include "connectivity/connectivity.hpp"
 #include "drivers/drivers.hpp"
 #include "expressions/expressions.hpp"
-#include "partitioned/partitioned.hpp"
+#include "fragmenting/fragmenting.hpp"
 #include "screening/screening.hpp"
 #include <ghostfragment/load_modules.hpp>
 
@@ -13,14 +13,14 @@ void load_modules(pluginplay::ModuleManager& mm) {
     connectivity::load_modules(mm);
     drivers::load_modules(mm);
     expressions::load_modules(mm);
-    partitioned::load_modules(mm);
+    fragmenting::load_modules(mm);
     screening::load_modules(mm);
 
     capping::set_defaults(mm);
     connectivity::set_defaults(mm);
     drivers::set_defaults(mm);
     expressions::set_defaults(mm);
-    partitioned::set_defaults(mm);
+    fragmenting::set_defaults(mm);
     screening::set_defaults(mm);
 }
 
