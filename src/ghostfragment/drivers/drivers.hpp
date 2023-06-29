8d855f0824bd3b367d67bc8d4ba9a9d5c8633e08
Isaac <115387963+ivanorman@users.noreply.github.com>
2023-06-29 13:48:44 -0500

    Re-enable fragment driver (#45)
    * new file:   src/ghostfragment/partitioned/bond_based.cpp
    	modified:   src/ghostfragment/partitioned/partitioned.hpp
    
    * modified:   src/ghostfragment/fragmenting/bond_based.cpp
    	new file:   tests/cxx/unit_tests/ghostfragment/fragmenting/bond_based.cpp
    
    * modified:   src/ghostfragment/fragmenting/bond_based.cpp
    	modified:   tests/cxx/unit_tests/ghostfragment/fragmenting/bond_based.cpp
    
    * modified:   CMakeLists.txt
    	modified:   src/ghostfragment/drivers/drivers.hpp
    	modified:   src/ghostfragment/drivers/fragment.cpp
    	modified:   src/ghostfragment/fragmenting/bond_based.cpp
    	modified:   src/ghostfragment/fragmenting/fragmenting.hpp
    	modified:   tests/cxx/unit_tests/ghostfragment/drivers/fragment.cpp
    	modified:   tests/cxx/unit_tests/ghostfragment/fragmenting/bond_based.cpp
    
    * modified:   src/ghostfragment/fragmenting/bond_based.cpp
    	modified:   tests/cxx/unit_tests/ghostfragment/fragmenting/bond_based.cpp
    
    * modified:   CMakeLists.txt
    
    * new file:   tests/cxx/integration_tests/ghostfragment/drivers/fragment.cpp
    	modified:   tests/cxx/unit_tests/ghostfragment/drivers/fragment.cpp
    
    * deleted:    tests/cxx/integration_tests/ghostfragment/drivers/fragment.cpp

--- src/ghostfragment/drivers/drivers.hpp
+++ src/ghostfragment/drivers/drivers.hpp
@@ -3,14 +3,14 @@
 
 namespace ghostfragment::drivers {
 
-// DECLARE_MODULE(Fragment);
+DECLARE_MODULE(Fragment);
 // DECLARE_MODULE(FragmentBasedMethod);
 // DECLARE_MODULE(FragmentedSystem);
 // DECLARE_MODULE(NMerSystem);
 
 /// Loads all the modules in the Drivers library into the provided ModuleManager
 inline void load_modules(pluginplay::ModuleManager& mm) {
-    // mm.add_module<Fragment>("Fragment Driver");
+    mm.add_module<Fragment>("Fragment Driver");
     // mm.add_module<FragmentBasedMethod>("Fragment Based Method");
     // mm.add_module<FragmentedSystem>("FragmentedSystem Driver");
     // mm.add_module<NMerSystem>("NMerSystem Driver");
@@ -19,11 +19,10 @@ inline void load_modules(pluginplay::ModuleManager& mm) {
 /// Sets the defaults for submodules in the Drivers library, when a suitable
 /// default is provided by GhostFragment
 inline void set_defaults(pluginplay::ModuleManager& mm) {
-    // mm.change_submod("Fragment Driver", "Pseudoatoms", "Heavy Atom
-    // Partition"); mm.change_submod("Fragment Driver", "Molecular graph",
-    // "Molecular graph"); mm.change_submod("Fragment Driver", "Molecular graph
-    // to fragments",
-    //                  "Cluster Partition");
+    mm.change_submod("Fragment Driver", "Pseudoatoms", "Heavy Atom Partition");
+    mm.change_submod("Fragment Driver", "Molecular graph", "Molecular Graph");
+    mm.change_submod("Fragment Driver", "Molecular graph to fragments",
+    "Bond-Based Fragmenter");
 
     // mm.change_submod("FragmentedSystem Driver", "Fragmenter",
     //                  "Fragment Driver");
--- src/ghostfragment/drivers/fragment.cpp
+++ src/ghostfragment/drivers/fragment.cpp
@@ -1,58 +1,59 @@
-// #include "drivers.hpp"
-// #include <ghostfragment/property_types/property_types.hpp>
+#include "drivers.hpp"
+#include <ghostfragment/property_types/property_types.hpp>
+#include <ghostfragment/property_types/fragmented_nuclei.hpp>
 
-// namespace ghostfragment::drivers {
+namespace ghostfragment::drivers {
 
-// using frags_pt       = simde::FragmentedMolecule;
-// using graph_pt       = pt::MolecularGraph;
-// using graph2frags_pt = pt::MolecularGraphToFragments;
+using frags_pt       = pt::FragmentedNuclei;
+using graph_pt       = pt::MolecularGraph;
+using graph2frags_pt = pt::MolecularGraphToFragments;
 
-// const auto mod_desc = R"(
-// Fragment Driver
-// ---------------
+const auto mod_desc = R"(
+Fragment Driver
+---------------
 
-// This module is responsible for going from a simde::type::molecule object to
-// a simde::type::fragmented_molecule object. Generally speaking this occurrs
-// by:
+This module is responsible for going from a simde::type::molecule object to
+a simde::type::fragmented_molecule object. Generally speaking this occurrs
+by:
 
-// #. Forming pseudoatoms
-// #. Determining the connectivity of the pseudoatoms
-// #. Fragmenting the resulting molecular graph
+#. Forming pseudoatoms
+#. Determining the connectivity of the pseudoatoms
+#. Fragmenting the resulting molecular graph
 
-// Ultimately, pseudoatoms are simply a partitioning of the molecule. In fact,
-// the pesudoatoms are (conceptually) perfectly fine fragments in and of
-// themselves. For this reason we take the submodule for forming the pseudoatoms
-// to be of type simde::FragmentMolecule also. Next we determine the
-// connectivity of the pseudoatoms; this happens in a submodule of type
-// MolecularGraph. Finally, the driver calls into a submodule of type
-// MolecularGraphToFragments, which is responsible for breaking the
-// MolecularGraph instance into fragments.
+Ultimately, pseudoatoms are simply a partitioning of the molecule. In fact,
+the pesudoatoms are (conceptually) perfectly fine fragments in and of
+themselves. For this reason we take the submodule for forming the pseudoatoms
+to be of type simde::FragmentMolecule also. Next we determine the
+connectivity of the pseudoatoms; this happens in a submodule of type
+MolecularGraph. Finally, the driver calls into a submodule of type
+MolecularGraphToFragments, which is responsible for breaking the
+MolecularGraph instance into fragments.
 
-// )";
+)";
 
-// MODULE_CTOR(Fragment) {
-//     description(mod_desc);
-//     satisfies_property_type<frags_pt>();
+MODULE_CTOR(Fragment) {
+    description(mod_desc);
+    satisfies_property_type<frags_pt>();
 
-//     add_submodule<frags_pt>("Pseudoatoms");
-//     add_submodule<graph_pt>("Molecular graph");
-//     add_submodule<graph2frags_pt>("Molecular graph to fragments");
-// }
+    add_submodule<frags_pt>("Pseudoatoms");
+    add_submodule<graph_pt>("Molecular graph");
+    add_submodule<graph2frags_pt>("Molecular graph to fragments");
+}
 
-// MODULE_RUN(Fragment) {
-//     const auto& [mol] = frags_pt::unwrap_inputs(inputs);
+MODULE_RUN(Fragment) {
+    const auto& [mol] = frags_pt::unwrap_inputs(inputs);
 
-//     auto& pseudoatom_mod      = submods.at("Pseudoatoms");
-//     const auto& [pseudoatoms] = pseudoatom_mod.run_as<frags_pt>(mol);
+    auto& pseudoatom_mod    = submods.at("Pseudoatoms");
+    const auto& pseudoatoms = pseudoatom_mod.run_as<frags_pt>(mol);
 
-//     auto& graph_mod     = submods.at("Molecular Graph");
-//     const auto& [graph] = graph_mod.run_as<graph_pt>(pseudoatoms);
+    auto& graph_mod         = submods.at("Molecular Graph");
+    const auto& graph       = graph_mod.run_as<graph_pt>(pseudoatoms);
 
-//     auto& frags_mod     = submods.at("Molecular graph to fragments");
-//     const auto& [frags] = frags_mod.run_as<graph2frags_pt>(graph);
+    auto& frags_mod         = submods.at("Molecular graph to fragments");
+    const auto& frags       = frags_mod.run_as<graph2frags_pt>(graph);
 
-//     auto rv = results();
-//     return frags_pt::wrap_results(rv, frags);
-// }
+    auto rv = results();
+    return frags_pt::wrap_results(rv, frags);
+}
 
-// } // namespace ghostfragment::drivers
+} // namespace ghostfragment::drivers
