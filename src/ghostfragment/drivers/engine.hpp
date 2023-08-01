8683df3749b149b91863eb8dadd6ebbb1ec21552
Walker Hayes <112342932+wthayes@users.noreply.github.com>
2023-08-01 13:58:15 -0500

    Engine (#57)
    * Engine created and tested.
    
    * Passing input by reference
    
    * Changed function to a header file

---
+++ src/ghostfragment/drivers/engine.hpp
@@ -0,0 +1,15 @@
+#include <simde/simde.hpp>
+#include <pluginplay/module.hpp>
+
+using input_type = std::vector<std::pair<chemist::ChemicalSystem, double>>;
+using pt = simde::Energy;
+using mod_type = pluginplay::Module;
+
+inline auto engine(const input_type& input, mod_type mod) {
+    auto weighted_sum = 0.0;
+    for(auto in : input) {
+        weighted_sum += mod.run_as<pt>(in.first) * in.second;
+    }
+
+    return weighted_sum;
+}
---
+++ tests/cxx/unit_tests/ghostfragment/drivers/engine.cpp
@@ -0,0 +1,91 @@
+#include "../test_ghostfragment.hpp"
+#include "hydrocarbon/hydrocarbon.h"
+#include <ghostfragment/drivers/engine.hpp>
+#include <ghostfragment/property_types/fragmented_molecule.hpp>
+#include <ghostfragment/property_types/fragmented_nuclei.hpp>
+#include <ghostfragment/property_types/property_types.hpp>
+
+using namespace ghostfragment;
+using namespace testing;
+
+using system_type = chemist::ChemicalSystem;
+using pair_type   = std::pair<system_type, double>;
+using input_type  = std::vector<pair_type>;
+using energy_type = simde::Energy;
+
+namespace {
+
+system_type methane(hydrocarbon(1));
+system_type ethane(hydrocarbon(2));
+system_type propane(hydrocarbon(3));
+
+auto make_lambda() {
+    return pluginplay::make_lambda<energy_type>([=](const auto& pair_in) {
+        if(pair_in == methane)
+            return 1.1;
+        else if(pair_in == ethane)
+            return -2.2;
+        else if(pair_in == propane)
+            return 3.3;
+        else
+            return 100.0;
+    });
+}
+
+} // namespace
+
+TEST_CASE("Engine") {
+    SECTION("Empty System") {
+        input_type input;
+        float test = engine(input, make_lambda());
+
+        float corr = 0.0;
+
+        REQUIRE(test == Approx(corr));
+    }
+
+    SECTION("One System") {
+        input_type input{{methane, 1.0}};
+        float test = engine(input, make_lambda());
+
+        float corr = 1.1;
+
+        REQUIRE(test == Approx(corr));
+    }
+
+    SECTION("Two Unique Systems") {
+        input_type input{{methane, 1.0}, {ethane, 2.0}};
+        float test = engine(input, make_lambda());
+
+        float corr = -3.3;
+
+        REQUIRE(test == Approx(corr));
+    }
+
+    SECTION("Two Equivalent Systems") {
+        input_type input{{methane, 1.0}, {methane, 2.0}};
+        float test = engine(input, make_lambda());
+
+        float corr = 3.3;
+
+        REQUIRE(test == Approx(corr));        
+    }
+
+    SECTION("Three Unique Systems") {
+        input_type input{{methane, 1.0}, {ethane, 2.0}, {propane, 3.0}};
+        float test = engine(input, make_lambda());
+
+        float corr = 6.6;
+
+        REQUIRE(test == Approx(corr));
+    }
+
+    SECTION("Three Systems with One Pair") {
+        input_type input{{methane, 1.0}, {methane, 2.0}, {propane, 3.0}};
+        float test = engine(input, make_lambda());
+
+        float corr = 13.2;
+
+        REQUIRE(test == Approx(corr));  
+    }
+}
